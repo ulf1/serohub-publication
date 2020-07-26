ccad7fd45c0530f72d258aca1803a57188a64440
UH <554c46@gmail.com>
2020-07-26 14:06:38 +0200

    argparse added

--- scripts/biorxiv-download.py
+++ scripts/biorxiv-download.py
@@ -2,17 +2,28 @@
 from datetime import datetime as dt
 import requests
 import os
+import argparse
+URL = "https://connect.biorxiv.org/relate/collection_json.php?grp=181"
+
 
 if __name__ == "__main__":
+    # process input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o", "--outputfile", type=str, default="data/raw/tmp/biorxiv.json",
+        help="full path where to store the biorxiv JSON file.")
+    parser.add_argument(
+        "-u", "--url", type=str, default=URL,
+        help="URL of biorxiv feed")
+    args = parser.parse_args()
+
     # Download the whole "SARS-CoV-2" feed
-    URL = "https://connect.biorxiv.org/relate/collection_json.php?grp=181"
-    resp = requests.get(URL)
-    print(f"[INFO] {dt.now()}: Latest biorxiv SARS-Cov-2 feed downloaded")
+    print(f"[INFO] {dt.now()}: Download started from '{args.url}'.")
+    resp = requests.get(args.url)
+    print(f"[INFO] {dt.now()}: Latest biorxiv SARS-Cov-2 feed downloaded.")
 
     # Store the response as JSON
-    FILEPATH = "data/raw/tmp"
-    FILENAME = "biorxiv.json"
-    os.makedirs(FILEPATH, exist_ok=True)
-    with open(os.path.join(FILEPATH, FILENAME), "wb") as fptr:
+    os.makedirs(os.path.dirname(args.outputfile), exist_ok=True)
+    with open(args.outputfile, "wb") as fptr:
         fptr.write(resp.content)
-    print(f"[INFO] {dt.now()}: Feed stored as {FILEPATH}")
+    print(f"[INFO] {dt.now()}: Feed stored as '{args.outputfile}'.")
